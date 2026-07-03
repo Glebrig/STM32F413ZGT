5946a1014d94db24ed93f7ef330a37a09c18993b
Glebrig <89831824+Glebrig@users.noreply.github.com>
2026-07-03 09:07:09 +0300

    v1.0
    Реализация взаимодействия с флеш-памятью через SPI + отладка через UART

---
+++ Core/Src/usart.c
@@ -0,0 +1,114 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    usart.c
+  * @brief   This file provides code for the configuration
+  *          of the USART instances.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "usart.h"
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+UART_HandleTypeDef huart10;
+
+/* UART10 init function */
+void MX_UART10_Init(void)
+{
+
+  /* USER CODE BEGIN UART10_Init 0 */
+
+  /* USER CODE END UART10_Init 0 */
+
+  /* USER CODE BEGIN UART10_Init 1 */
+
+  /* USER CODE END UART10_Init 1 */
+  huart10.Instance = UART10;
+  huart10.Init.BaudRate = 115200;
+  huart10.Init.WordLength = UART_WORDLENGTH_8B;
+  huart10.Init.StopBits = UART_STOPBITS_1;
+  huart10.Init.Parity = UART_PARITY_NONE;
+  huart10.Init.Mode = UART_MODE_TX_RX;
+  huart10.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart10.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart10) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN UART10_Init 2 */
+
+  /* USER CODE END UART10_Init 2 */
+
+}
+
+void HAL_UART_MspInit(UART_HandleTypeDef* uartHandle)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  if(uartHandle->Instance==UART10)
+  {
+  /* USER CODE BEGIN UART10_MspInit 0 */
+
+  /* USER CODE END UART10_MspInit 0 */
+    /* UART10 clock enable */
+    __HAL_RCC_UART10_CLK_ENABLE();
+
+    __HAL_RCC_GPIOE_CLK_ENABLE();
+    /**UART10 GPIO Configuration
+    PE2     ------> UART10_RX
+    PE3     ------> UART10_TX
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_2|GPIO_PIN_3;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF11_UART10;
+    HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN UART10_MspInit 1 */
+
+  /* USER CODE END UART10_MspInit 1 */
+  }
+}
+
+void HAL_UART_MspDeInit(UART_HandleTypeDef* uartHandle)
+{
+
+  if(uartHandle->Instance==UART10)
+  {
+  /* USER CODE BEGIN UART10_MspDeInit 0 */
+
+  /* USER CODE END UART10_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_UART10_CLK_DISABLE();
+
+    /**UART10 GPIO Configuration
+    PE2     ------> UART10_RX
+    PE3     ------> UART10_TX
+    */
+    HAL_GPIO_DeInit(GPIOE, GPIO_PIN_2|GPIO_PIN_3);
+
+  /* USER CODE BEGIN UART10_MspDeInit 1 */
+
+  /* USER CODE END UART10_MspDeInit 1 */
+  }
+}
+
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
+
